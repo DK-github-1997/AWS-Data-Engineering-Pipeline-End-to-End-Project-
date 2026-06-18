36ea0eb181b68f2a8548d4237813b5b93165c57a
Dnyaneshvar Khairnar <155963360+DK-github-1997@users.noreply.github.com>
2026-06-18 16:23:05 +0530

    Add header comments to glue_job_scd1.py
    Added header comments for SCD Type 1 code, including author and date.

--- glue_job_scd1.py
+++ glue_job_scd1.py
@@ -1,4 +1,6 @@
-
+## SCD Type 1 Code ##
+## Author By DK1997 
+## 18 Jun 2026 
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("SCD1").getOrCreate()
 df = spark.read.parquet("s3://bucket/processed/")
